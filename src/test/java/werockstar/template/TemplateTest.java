ba7c366a3a111370f21ae4f4ac3dd643b883bf34
WeRockStar <kotchaphan.m@gmail.com>
2017-07-15 22:14:06 +0700

    Refactor duplicate test

--- src/test/java/werockstar/template/TemplateTest.java
+++ src/test/java/werockstar/template/TemplateTest.java
@@ -1,47 +1,39 @@
 package werockstar.template;
 
 import com.werockstar.template.Template;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TemplateTest {
 
+    private Template template;
+
+    @Before
+    public void setUp() throws Exception {
+        template = new Template("${one}, ${two}, ${three}");
+        template.set("one", "1");
+        template.set("two", "2");
+        template.set("three", "3");
+    }
+
     @Test
     public void unknown_variable_should_ignored() throws Exception {
-        Template template = new Template("Hello, ${name}");
-        template.set("name", "WeRockStar");
-        template.set("unknown", "Any");
-        assertEquals("Hello, WeRockStar", template.getEvaluate());
-    }
-
-    @Test
-    public void should_see_reader_be_Reader() throws Exception {
-        Template template = new Template("Hello, ${name}");
-        template.set("name", "Reader");
-        assertEquals("Hello, Reader", template.getEvaluate());
-    }
-
-    @Test
-    public void should_see_reader_be_someone_else() throws Exception {
-        Template template = new Template("Hello, ${name}");
-        template.set("name", "someone else");
-        assertEquals("Hello, someone else", template.getEvaluate());
+        template.set("unknown", "ignored");
+        assertEquals("1, 2, 3", template.getEvaluate());
     }
 
     @Test
     public void should_see_reader_be_WeRockStar() throws Exception {
-        Template template = new Template("Hello, ${name}");
+        template = new Template("Hello, ${name}");
         template.set("name", "WeRockStar");
+        template.set("unknown", "ignored");
         assertEquals("Hello, WeRockStar", template.getEvaluate());
     }
 
     @Test
     public void add_multiple_variable() throws Exception {
-        Template template = new Template("${one}, ${two}, ${three}");
-        template.set("one", "1");
-        template.set("two", "2");
-        template.set("three", "3");
         assertEquals("1, 2, 3", template.getEvaluate());
     }
 }
