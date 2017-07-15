ebdbd0d778a43331d267ebcce3ab675f161904d3
WeRockStar <kotchaphan.m@gmail.com>
2017-07-15 20:57:29 +0700

    Make it pass

--- src/main/java/com/werockstar/template/Template.java
+++ src/main/java/com/werockstar/template/Template.java
@@ -1,14 +1,17 @@
 package com.werockstar.template;
 
 public class Template {
+
+    private String value;
+
     public Template(String template) {
     }
 
     public void set(String variable, String value) {
-
+        this.value = value;
     }
 
     public String getEvaluate() {
-        return "Hello, Reader";
+        return "Hello, " + value;
     }
 }
--- src/test/java/werockstar/template/TemplateTest.java
+++ src/test/java/werockstar/template/TemplateTest.java
@@ -13,4 +13,11 @@ public class TemplateTest {
         template.set("name", "Reader");
         assertEquals("Hello, Reader", template.getEvaluate());
     }
+
+    @Test
+    public void difference_variable() throws Exception {
+        Template template = new Template("Hello, ${name}");
+        template.set("name", "someone else");
+        assertEquals("Hello, someone else", template.getEvaluate());
+    }
 }
