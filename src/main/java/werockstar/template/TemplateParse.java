222950ee30fa1a18f138b62c30381f10ab98506f
WeRockStar <kotchaphan.m@gmail.com>
2017-07-16 14:24:27 +0700

    Refactor parse duplicate

--- src/main/java/werockstar/template/TemplateParse.java
+++ src/main/java/werockstar/template/TemplateParse.java
@@ -1,13 +1,11 @@
 package werockstar.template;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TemplateParse {
-    private List<String> segments = new ArrayList<>();
 
-    public List<String> parse(String segment) {
-        this.segments.add(segment);
-        return this.segments;
+    public List<String> parse(String template) {
+        return Collections.singletonList(template);
     }
 }
--- src/test/java/werockstar/template/TemplateParseTest.java
+++ src/test/java/werockstar/template/TemplateParseTest.java
@@ -8,11 +8,21 @@ import static org.junit.Assert.assertEquals;
 
 public class TemplateParseTest {
 
+    private List<String> parse(String template) {
+        return new TemplateParse().parse(template);
+    }
+
     @Test
     public void empty_template_renders_as_empty_string() throws Exception {
-        TemplateParse parse = new TemplateParse();
-        List<String> segments = parse.parse("");
+        List<String> segments = parse("");
         assertEquals("Number of segments", 1, segments.size());
         assertEquals("", segments.get(0));
+    }
+
+    @Test
+    public void template_with_only_plain_text() throws Exception {
+        List<String> segments = parse("plain text only");
+        assertEquals("Number of segments", 1, segments.size());
+        assertEquals("plain text only", segments.get(0));
     }
 }
